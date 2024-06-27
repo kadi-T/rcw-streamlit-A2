15075bf597f63b14442d0192cef4ba8ce471bd71
kadi-T <156639209+kadi-T@users.noreply.github.com>
2024-06-27 12:12:46 -0400

    Affichage des donnees de la table etudiants depuis snowflake sur streamlit

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 import streamlit as st
-
+import snowflake.connector as sc
+import pandas as pd
 
 def main():
     st.write("### Dashbord d'affichage des donnees de la table etudiants")
+    
+    # objet de connection
+    try:
+        
+        con  = sc.connect(
+            account ='znejkha-ui11118',
+             user = 'Kadi',
+            password = 'OUktra20022023'
+        )
+        cursor = con.cursor()
+        
+        def dataEtudiants():
+            sql = 'SELECT * FROM rcw.persons.etudiants'
+            df = cursor.execute(sql).fetchall()
+            
+            return pd.DataFrame(df, columns  = ['NOM', 'PRENOM', 'AGE'])
+        donnees = dataEtudiants()
+        st.write(donnees)
+        
+        
+    except:
+        st.warning("Une erreur c'est produit")
+    
+    
 
 
 
